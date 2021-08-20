fa0ec242580b9224bb7372e96d7af298a6626154
Hamza Saadi <36114417+sakaspar@users.noreply.github.com>
2021-08-20 21:27:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,56 +10,55 @@ import JoditEditor from "jodit-react";
 function App() {
   const editor = useRef(null)
     const [content, setContent] = useState('')
-
+    const [send, setSend] = useState('')
     const config = {
-        readonly: false // documentation : https://xdsoft.net/jodit/doc/
+        readonly: false 
+        
     }
   return (
-    <div className="App">
+<div className="App">
         <div className='Left__Part'>
          <div className='Left__Part_main'>
           <div className='Left__Part__Title'>
             <div className='Left__Part__Title__Title'>Customisation</div>
             <div className='Left__Part__Title__SubTitle'>The text will reflect mobile view</div>
             <br></br>
-         </div>
+          </div>
         <div className='Left__Part__Editor'> 
-      <Accordion className='Summery'>
-        <AccordionSummary className='slidebartext'
+      
+      <Accordion  >
+      <div className='Summery1' >
+        <AccordionSummary className='slidebartext' 
          expandIcon={<ExpandMoreIcon />}
          aria-controls="panel1a-content"
          id="panel1a-header">Text
         </AccordionSummary>
+        </div>
         <JoditEditor 
           ref={editor}
           value={content}
           config={config}
           tabIndex={1} // tabIndex of textarea
           editor={FocusEvent}
-          //onChange={content => setContent(content)}
           onBlur={newContent => setContent(newContent)} 
         />
       </Accordion>
-      </div>
-      <div className='Left__Part__Publish__Btn'> 
-      <button   className='Left__Part__Publish__Btn__Style' >Publish {'>'}</button>
+       </div>
+        <div className='Left__Part__Publish__Btn'> 
+          <button   className='Left__Part__Publish__Btn__Style' onClick={send => setSend(content)} >Publish {'>'}</button>
+        </div>
       </div>
     </div>
-  </div>
-        
-        {console.log({content})}
-       
   <div className='Right__Part'>
     <div className='containerIphone'>
       <div className='textarea'> 
         <div className='Header'>Text</div>
-          <div className="top-left" dangerouslySetInnerHTML={{__html:content}} />
+          <div className="top-left" dangerouslySetInnerHTML={{__html:send}} />
        </div>
         <img src={iphone} alt="Logo" className="Right__Part__Image"/>
       </div>    
     </div>
-   </div>
-     
+   </div> 
   );
 }
 
